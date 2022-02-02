62da6426fdf0cfbed52bf72ef12e59491a840498
Sandi <sandi.dusic@protonmail.com>
2022-02-02 23:47:09 +0000

    ekjkdlas

--- src/Main.elm
+++ src/Main.elm
@@ -1,15 +1,51 @@
 module Main exposing (main)
 
-import Html exposing (Html)
-import Html.Attributes
+import Browser
+import Browser.Navigation as Nav
+import Html exposing (..)
+import Time
 
 
-main : Html a
+type Msg
+    = NoOp
+    | NewTime Time.Posix
+
+
+main : Program {} Time.Posix Msg
 main =
-    Html.section []
-        [ Html.h1 [] [ Html.text "Minimal Elm + Parcel implementation, deployed to Netlify" ]
-        , Html.p []
-            [ Html.text "Find the source on "
-            , Html.a [ Html.Attributes.href "https://github.com/JoelQ/elm-netlify-parcel" ] [ Html.text "GitHub" ]
+    Browser.application
+        { init = \_ _ _ -> ( Time.millisToPosix 0, Cmd.none )
+        , view = view
+        , update =
+            \msg model ->
+                case msg of
+                    NewTime newTime ->
+                        ( newTime, Cmd.none )
+
+                    _ ->
+                        ( model, Cmd.none )
+        , subscriptions = always <| Time.every 1000 NewTime
+        , onUrlRequest = always NoOp
+        , onUrlChange = always NoOp
+        }
+
+
+view currentTime =
+    let
+        birthday =
+            803779200000
+
+        dt =
+            Time.posixToMillis currentTime - birthday
+
+        in_years =
+            toFloat dt / (1000 * 60 * 60 * 24 * 365)
+    in
+    { title = "hey"
+    , body =
+        [ Html.main_ []
+            [ Html.div []
+                [ Html.text <| String.fromFloat <| in_years ]
             ]
         ]
+    }
